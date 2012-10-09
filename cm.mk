455e5b974a0c6c7e9fd6f24b860e8dbf046bff08
lirokoa <lirokoa@hotmail.com>
2012-10-09 18:58:28 +0200

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,22 @@
+# Prepare for new BootAnimation
+TARGET_BOOTANIMATION_NAME := vertical-320x480
+
+# Include GSM stuff
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/pico/pico.mk)
+
+
+# Release name
+PRODUCT_RELEASE_NAME := pico
+
+PRODUCT_NAME := cm_pico
+PRODUCT_BRAND := HTC
+PRODUCT_DEVICE := pico
+PRODUCT_MODEL := HTC Explorer A310e
+PRODUCT_MANUFACTURER := HTC
+ 
